37a015933976665ade8b15634aee6547569204d7
Kamouth <camille.vaccalut@gmail.com>
2014-04-27 17:21:28 -0400

    amelioration de l'interface de consultation des détails d'un tournoi

--- gen/com/example/projetift2905/R.java
+++ gen/com/example/projetift2905/R.java
@@ -9,9 +9,12 @@ package com.example.projetift2905;
 
 public final class R {
     public static final class array {
-        public static final int games=0x7f060000;
+        public static final int games=0x7f070000;
     }
     public static final class attr {
+    }
+    public static final class color {
+        public static final int BackGroundColor=0x7f050000;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -28,24 +31,33 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int ValiderCreerTournoi=0x7f090004;
-        public static final int action_settings=0x7f090011;
-        public static final int buttonToCreerTournoi=0x7f09000c;
-        public static final int buttonToDetailsTournoi=0x7f09000b;
-        public static final int container=0x7f090000;
-        public static final int editNomNewTournoi=0x7f090002;
-        public static final int favoriButton=0x7f09000f;
-        public static final int listTournois=0x7f09000e;
-        public static final int nomTournoi=0x7f090010;
-        public static final int pager=0x7f09000d;
-        public static final int spinner1=0x7f090003;
-        public static final int textView1=0x7f090005;
-        public static final int textView2=0x7f090001;
-        public static final int textView3=0x7f090007;
-        public static final int textView4=0x7f090009;
-        public static final int textView5=0x7f090006;
-        public static final int textView6=0x7f090008;
-        public static final int textView7=0x7f09000a;
+        public static final int ValiderCreerTournoi=0x7f0a0004;
+        public static final int action_settings=0x7f0a001a;
+        public static final int button1=0x7f0a0013;
+        public static final int button2=0x7f0a0012;
+        public static final int buttonToCreerTournoi=0x7f0a0015;
+        public static final int buttonToDetailsTournoi=0x7f0a0014;
+        public static final int container=0x7f0a0000;
+        public static final int editNomNewTournoi=0x7f0a0002;
+        public static final int favoriButton=0x7f0a0018;
+        public static final int imageView1=0x7f0a0006;
+        public static final int listTournois=0x7f0a0017;
+        public static final int nomTournoi=0x7f0a0019;
+        public static final int pager=0x7f0a0016;
+        public static final int spinner1=0x7f0a0003;
+        public static final int textView1=0x7f0a0005;
+        public static final int textView10=0x7f0a000e;
+        public static final int textView11=0x7f0a000f;
+        public static final int textView12=0x7f0a0010;
+        public static final int textView13=0x7f0a0011;
+        public static final int textView2=0x7f0a0001;
+        public static final int textView3=0x7f0a0008;
+        public static final int textView4=0x7f0a000a;
+        public static final int textView5=0x7f0a0007;
+        public static final int textView6=0x7f0a0009;
+        public static final int textView7=0x7f0a000b;
+        public static final int textView8=0x7f0a000c;
+        public static final int textView9=0x7f0a000d;
     }
     public static final class layout {
         public static final int activity_creer_tournoi=0x7f030000;
@@ -57,25 +69,25 @@ public final class R {
         public static final int tournoi_element=0x7f030006;
     }
     public static final class menu {
-        public static final int creer_tournoi=0x7f080000;
-        public static final int details_tournoi=0x7f080001;
-        public static final int main=0x7f080002;
+        public static final int creer_tournoi=0x7f090000;
+        public static final int details_tournoi=0x7f090001;
+        public static final int main=0x7f090002;
     }
     public static final class string {
-        public static final int DOTA2=0x7f05000b;
-        public static final int InfoTourneyId=0x7f050008;
-        public static final int LoL=0x7f05000a;
-        public static final int action_settings=0x7f050001;
-        public static final int api_vide=0x7f050006;
-        public static final int app_name=0x7f050000;
+        public static final int DOTA2=0x7f06000b;
+        public static final int InfoTourneyId=0x7f060008;
+        public static final int LoL=0x7f06000a;
+        public static final int action_settings=0x7f060001;
+        public static final int api_vide=0x7f060006;
+        public static final int app_name=0x7f060000;
         /**  Ressources pour MainActivity 
          */
-        public static final int title_activity_=0x7f050002;
-        public static final int title_activity_creer_tournoi=0x7f050009;
-        public static final int title_activity_details_tournoi=0x7f050007;
-        public static final int title_section1=0x7f050003;
-        public static final int title_section2=0x7f050004;
-        public static final int title_section3=0x7f050005;
+        public static final int title_activity_=0x7f060002;
+        public static final int title_activity_creer_tournoi=0x7f060009;
+        public static final int title_activity_details_tournoi=0x7f060007;
+        public static final int title_section1=0x7f060003;
+        public static final int title_section2=0x7f060004;
+        public static final int title_section3=0x7f060005;
     }
     public static final class style {
         /** 
@@ -99,10 +111,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f080001;
     }
 }
